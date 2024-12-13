7ae1f8352f38229f69c13e9093ecba83447e5474
jaegeon.jo <jaegeon.jo@murple.ai>
2024-12-13 19:02:16 +0900

    보더 디자인 추가, README 정리

--- src/components/InputText.vue
+++ src/components/InputText.vue
@@ -10,7 +10,7 @@ defineProps<InputTextProps>();
   <label class="flex flex-col gap-4">
     {{ label }}
     <input
-      class="border-blue-400 border rounded-md h-10 px-4"
+      class="border-gray-400 border rounded-md h-10 px-4 hover:border-blue-400 invalid:border-red-500 focus:border-green-400 focus:outline-none"
       type="text"
       autocomplete="off"
       v-model="model"
